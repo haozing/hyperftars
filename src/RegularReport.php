7efee054962710c40c0995694d6047d8a6756c16
haozing <570193025@qq.com>
2020-12-21 17:46:58 +0800

    bug

--- src/RegularReport.php
+++ src/RegularReport.php
@@ -28,18 +28,7 @@ class RegularReport
         $application = $getTarsConf['app'];
         $this->logger->info("定时上报：".$application.$serverName);
         \Swoole\Timer::tick(10000, function() use ($serverName, $application) {
-            //获取当前存活的worker数目
-            $processName = $application . '.' . $serverName;
-            $cmd = "ps wwaux | grep '" . $processName . "' | grep 'event worker process' | grep -v grep  | awk '{ print $2}'";
-            exec($cmd, $ret);
-            $workerNum = count($ret);
-
-            if ($workerNum >= 1) {
-                $this->container->get(TarsPlatform::class)->keepaliveReport();
-            } //worker全挂，不上报存活 等tars重启
-            else {
-                $this->logger->error(__METHOD__ . " All workers are not alive any more.");
-            }
+            $this->container->get(TarsPlatform::class)->keepaliveReport();
         });
     }
 }
